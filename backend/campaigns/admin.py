4376ff10ece035c71a5d178e9911f24f867691c0
Babe Shark <xratedrbw@gmail.com>
2021-11-03 16:31:32 -0600

    first commit

---
+++ backend/campaigns/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Campaign, Subscriber
+# Register your models here.
+
+class CampaignModelAdmin(admin.ModelAdmin):
+    list_display=['title','created_at','updated_at']
+    search_fields= ('title','description')
+    list_per_page=5
+
+class SubscriberModelAdmin(admin.ModelAdmin):
+    list_display=['email','campaign','created_at']
+    search_fields= ('email','campaign__title','created_at')
+    list_per_page=5
+
+admin.site.register(Campaign,CampaignModelAdmin)
+admin.site.register(Subscriber,SubscriberModelAdmin)
